613d7d283a414d9b19642403004e595695726284
FabricioFBar <75705088+FabricioFBar@users.noreply.github.com>
2025-04-05 10:03:46 -0300

    Update server.go

--- server.go
+++ server.go
@@ -3,13 +3,28 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
+    "time"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hellow World Students")
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+    // Mensagem principal
+    fmt.Fprintf(w, "<h1>Olá, teste de aplicação GO no OpenShift</h1>")
+
+    // Data e hora de execução
+    currentTime := time.Now().Format("02/01/2006 15:04:05")
+    fmt.Fprintf(w, "<p><strong>Data de execução:</strong> %s</p>", currentTime)
+
+    // Nome do servidor (hostname)
+    hostname, err := os.Hostname()
+    if err != nil {
+        hostname = "Servidor desconhecido"
+    }
+    fmt.Fprintf(w, "<p><strong>Servidor:</strong> %s</p>", hostname)
 }
 
 func main() {
     http.HandleFunc("/", helloWorld)
+    fmt.Println("Servidor iniciado em http://localhost:8080")
     http.ListenAndServe(":8080", nil)
 }
